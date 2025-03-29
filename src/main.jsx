295afaa7b4cfdc2fab53bf0630198beaf408b700
pranayAkarapu <pranayakarapu@gmail.com>
2025-03-29 23:18:36 +0530

    second-commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,17 @@ import { createRoot } from 'react-dom/client'
 //import './index.css'
 import App from './App.jsx'
 import UsersList from './components/UsersList.jsx'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
-const routers =  createBrowserRouter([
-  {
-    path:"/",
-    element: <App />,
-  },
-  {
-    path:"/users",
-    element:<UsersList/>
-  }
-])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={routers} />
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/users' element={<UsersList/>}/>
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 )
